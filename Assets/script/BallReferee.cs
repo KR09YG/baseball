bc56127800410a80df2dd2b7bf44e695913ced3e
KRYG <abcdef0509a@gmail.com>
2024-06-23 18:41:05 +0900

    アウトカウントの実装

--- Assets/script/BallReferee.cs
+++ Assets/script/BallReferee.cs
@@ -5,11 +5,13 @@ public class BallReferee : MonoBehaviour
     [SerializeField] Referee _referee;
 
     Batcollison _bat;
+    Out ball;
     private void Start()
     {
         _bat = GameObject.Find("baseball_bat").GetComponent<Batcollison>();
-;       
+         ball = GameObject.Find("Gamemanager").GetComponent<Out>();
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (_bat.boolcollison ==true)
@@ -17,12 +19,13 @@ public class BallReferee : MonoBehaviour
             Debug.Log("aaa");
             _referee.ImageChange(3);
             _bat.boolcollison=false;
-
+            
         }
         else
         {
             _referee.ImageChange(1);
             Destroy(collision.gameObject);
+            ball._ballcount +=1;
         }
         
     }
--- Assets/script/Out.cs
+++ Assets/script/Out.cs
@@ -10,16 +10,27 @@ public class Out : MonoBehaviour
     public int _ballcount = 0;
     SpriteRenderer _strickimage;
     SpriteRenderer _strickimage2;
+    SpriteRenderer _outimage;
+    SpriteRenderer _outimage2;
+    SpriteRenderer _ballimage;
+    SpriteRenderer _ballimage2;
+    SpriteRenderer _ballimage3;
     // Start is called before the first frame update
     void Start()
     {
         _strickimage = GameObject.Find("strick1").GetComponent<SpriteRenderer>();
         _strickimage2 = GameObject.Find("strick2").GetComponent<SpriteRenderer>();
+        _outimage = GameObject.Find("out").GetComponent<SpriteRenderer>();
+        _outimage2 = GameObject.Find("out2").GetComponent<SpriteRenderer>();
+        _ballimage = GameObject.Find("ball").GetComponent<SpriteRenderer>();
+        _ballimage2 = GameObject.Find("ball2").GetComponent<SpriteRenderer>();
+        _ballimage3 = GameObject.Find("ball3").GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(_ballcount);
         if (_strickcount >= 1)
         {
             _strickimage.color = new Color(255, 100, 0);
@@ -28,7 +39,56 @@ public class Out : MonoBehaviour
         {
             _strickimage.color = Color.white;
         }
-        Debug.Log(_strickcount);
+        if (_strickcount >= 2)
+        {
+            _strickimage2.color = new Color(255, 100, 0);
+        }
+        else
+        {
+            _strickimage2.color = Color.white;
+        }
+        if (_outcount >= 1)
+        {
+            _outimage.color = new Color(255, 0, 0);
+        }
+        else
+        {
+            _outimage.color = Color.white;
+        }
+        if (_outcount >= 2)
+        {
+            _outimage2.color = new Color(255, 0, 0);
+        }
+        else
+        {
+            _outimage2.color = Color.white;
+        }
+        if (_ballcount >= 1)
+        {
+            _ballimage.color = new Color(0, 255, 0);
+        }
+        else
+        {
+            _ballimage.color = Color.white;
+        }
+        if (_ballcount >= 2)
+        {
+            _ballimage2.color = new Color(0, 255, 0);
+        }
+        else
+        {
+            _ballimage2.color = Color.white;
+        }
+        if (_ballcount >= 3)
+        {
+            _ballimage3.color = new Color(0, 255, 0);
+        }
+        else
+        {
+            _ballimage3.color = Color.white;
+        }
+
+        
         if(_outcount > 2 )
         {
             
@@ -40,6 +100,11 @@ public class Out : MonoBehaviour
         if(_strickcount > 2)
         {
             _outcount += 1;
+            _strickcount = 0;
+        }
+        if ( _ballcount > 3 )
+        {
+            _ballcount = 0;
             _strickcount = 0;
         }
     }
